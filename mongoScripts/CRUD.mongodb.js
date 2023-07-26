948cb2554c82e44ea59908718bf15c8f4445b3fe
Kyrylo <fresh.js.ftd.@gmail.com>
2023-07-26 18:46:19 +0300

    Add Read query examples

--- mongoScripts/CRUD.mongodb.js
+++ mongoScripts/CRUD.mongodb.js
@@ -21,9 +21,62 @@ db.users.insertMany([
 ]);
 
 db.users.insertOne({
-  name: "user 3",
+  name: 'user 3',
   address: {
     country: 'UA',
-    city: "ZP",
-  }
+    city: 'ZP',
+  },
 });
+
+db.inventory.insertMany([
+  { item: 'journal', qty: 25, size: { h: 14, w: 21, uom: 'cm' }, status: 'A' },
+  {
+    item: 'notebook',
+    qty: 50,
+    size: { h: 8.5, w: 11, uom: 'in' },
+    status: 'A',
+  },
+  { item: 'paper', qty: 100, size: { h: 8.5, w: 11, uom: 'in' }, status: 'D' },
+  {
+    item: 'planner',
+    qty: 75,
+    size: { h: 22.85, w: 30, uom: 'cm' },
+    status: 'D',
+  },
+  {
+    item: 'postcard',
+    qty: 45,
+    size: { h: 10, w: 15.25, uom: 'cm' },
+    status: 'A',
+  },
+]);
+
+// Read
+db.inventory.find(); // SELECT * FROM inventory
+
+// SELECT * FROM inventory WHERE status = 'A'
+db.inventory.find({ status: 'A' });
+
+// SELECT * FROM inventory WHERE qty > 48
+db.inventory.find({ qty: { $gt: 48 } });
+
+// SELECT * FROM inventory WHERE size.uom = 'in'
+db.inventory.find({ 'size.uom': 'in' });
+
+// SELECT * FROM inventory WHERE size.uom = 'in' AND status = 'A' AND qty > 48
+db.inventory.find({ 'size.uom': 'in', status: 'A', qty: { $gt: 48 } });
+
+// SELECT * FROM inventory WHERE (status = A) OR size.h > 15
+db.inventory.find({ $or: [{ status: 'A' }, { 'size.h': { $gt: 15 } }] });
+
+db.users.find();
+
+// всі користувачі у кого є емейл
+db.users.find({ email: { $exists: true } });
+db.users.find({ email: { $exists: 1 } }); // true
+db.users.find({ email: { $exists: 0 } }); // false
+
+// 1. h > 9 , w <= 20
+db.inventory.find({ 'size.h': { $gt: 9 }, 'size.w': { $lte: 20 } });
+// 2. uom = 'cm', qty < 70
+db.inventory.find({ $and: [{ 'size.uom': 'cm' }, { qty: { $lt: 70 } }] });
