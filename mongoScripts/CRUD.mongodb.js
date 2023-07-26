2e35c8a094b7e308448dfdf1a71d181f590fc5d0
Kyrylo <fresh.js.ftd.@gmail.com>
2023-07-26 19:08:17 +0300

    Add Update queries examples

--- mongoScripts/CRUD.mongodb.js
+++ mongoScripts/CRUD.mongodb.js
@@ -80,3 +80,34 @@ db.users.find({ email: { $exists: 0 } }); // false
 db.inventory.find({ 'size.h': { $gt: 9 }, 'size.w': { $lte: 20 } });
 // 2. uom = 'cm', qty < 70
 db.inventory.find({ $and: [{ 'size.uom': 'cm' }, { qty: { $lt: 70 } }] });
+
+// Update
+db.inventory.insertMany( [
+  { item: "canvas", qty: 100, size: { h: 28, w: 35.5, uom: "cm" }, status: "A" },
+  { item: "journal", qty: 25, size: { h: 14, w: 21, uom: "cm" }, status: "A" },
+  { item: "mat", qty: 85, size: { h: 27.9, w: 35.5, uom: "cm" }, status: "A" },
+  { item: "mousepad", qty: 25, size: { h: 19, w: 22.85, uom: "cm" }, status: "P" },
+  { item: "notebook", qty: 50, size: { h: 8.5, w: 11, uom: "in" }, status: "P" },
+  { item: "paper", qty: 100, size: { h: 8.5, w: 11, uom: "in" }, status: "D" },
+  { item: "planner", qty: 75, size: { h: 22.85, w: 30, uom: "cm" }, status: "D" },
+  { item: "postcard", qty: 45, size: { h: 10, w: 15.25, uom: "cm" }, status: "A" },
+  { item: "sketchbook", qty: 80, size: { h: 14, w: 21, uom: "cm" }, status: "A" },
+  { item: "sketch pad", qty: 95, size: { h: 22.85, w: 30.5, uom: "cm" }, status: "A" }
+] );
+
+// один запис - updateOne
+// багато записів - updateMany
+
+// UPDATE inventory SET status = "Accepted" WHERE status = 'A'
+db.inventory.updateMany({status : 'A'}, { $set: { status: 'Accepted' }});
+
+db.inventory.find({status: 'Accepted'});
+
+// P -> Processing
+db.inventory.updateMany({status : 'P'}, { $set: { status: 'Processing' }});
+// D -> Done
+db.inventory.updateMany({status : 'D'}, { $set: { status: 'Done' }});
+
+// Delete
+
+db.inventory.drop() // DELETE FROM users;
