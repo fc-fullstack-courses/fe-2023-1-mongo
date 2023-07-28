f5928ad7d0665c902e7d8e57ccbbb5cfc39613b4
Kyrylo <fresh.js.ftd.@gmail.com>
2023-07-28 19:34:21 +0300

    Add timestamps and default values examples

--- server/models/User.js
+++ server/models/User.js
@@ -3,30 +3,33 @@ const yup = require('yup');
 
 const EMAIL_VALIDATION_SCHEMA = yup.string().email().required();
 
-const userSchema = new Schema({
-  firstName: {
-    type: String,
-    required: [true, 'First name is required'],
-    match: /^[A-Z][a-z]{0,32}$/,
-  },
-  lastName: {
-    type: String,
-    required: [true, 'Last name is required'],
-    match: /^[A-Z][a-z]{0,32}$/,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    validate: {
-      validator: async (value) => EMAIL_VALIDATION_SCHEMA.isValid(value),
-      message: 'Email is invalid',
+const userSchema = new Schema(
+  {
+    firstName: {
+      type: String,
+      required: [true, 'First name is required'],
+      match: /^[A-Z][a-z]{0,32}$/,
     },
+    lastName: {
+      type: String,
+      required: [true, 'Last name is required'],
+      match: /^[A-Z][a-z]{0,32}$/,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      validate: {
+        validator: async (value) => EMAIL_VALIDATION_SCHEMA.isValid(value),
+        message: 'Email is invalid',
+      },
+    },
+    password: { type: String, required: true },
+    birthday: { type: Date, default: Date.now },
+    isMale: { type: Boolean, default: true },
   },
-  password: { type: String, required: true },
-  birthday: { type: Date },
-  isMale: { type: Boolean },
-});
+  { timestamps: true }
+);
 
 const User = model('User', userSchema);
 
