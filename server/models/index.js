005ed29b6d6597f6d750712161fe9a869419fd03
Kyrylo <fresh.js.ftd.@gmail.com>
2023-07-28 18:42:44 +0300

    Fix mongoose db connection string

--- server/models/index.js
+++ server/models/index.js
@@ -4,7 +4,7 @@ const User = require('./User');
 async function connectToDB() {
   const USER_LOGIN = process.env.USER_LOGIN || 'admin';
   const USER_PASSWORD = process.env.USER_PASSWORD || 'admin';
-
+  const DB_HOST = process.env.DB_HOST || 'mongotest.yuttpkc.mongodb.net';
   const DB_URL =
     process.env.DB_URL ||
     `mongodb+srv://${USER_LOGIN}:${USER_PASSWORD}@${DB_HOST}`;
