79d857095875e3676b323b01f4249439dbfad923
Kyrylo <fresh.js.ftd.@gmail.com>
2023-07-28 19:10:01 +0300

    Add basic mongoose schema validation

--- server/models/User.js
+++ server/models/User.js
@@ -1,12 +1,31 @@
 const { Schema, model } = require('mongoose');
+const yup = require('yup');
+
+const EMAIL_VALIDATION_SCHEMA = yup.string().email().required();
 
 const userSchema = new Schema({
-  firstName: String,
-  lastName: String,
-  email: String,
-  password: String,
+  firstName: {
+    type: String,
+    required: [true, 'First name is required'],
+    match: /^[A-Z][a-z]{0,32}$/,
+  },
+  lastName: {
+    type: String,
+    required: [true, 'Last name is required'],
+    match: /^[A-Z][a-z]{0,32}$/,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator: async (value) => EMAIL_VALIDATION_SCHEMA.isValid(value),
+      message: 'Email is invalid',
+    },
+  },
+  password: { type: String, required: true },
   birthday: { type: Date },
-  isMale: Boolean,
+  isMale: { type: Boolean },
 });
 
 const User = model('User', userSchema);
