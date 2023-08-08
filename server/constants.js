4e9173f8e4d251389e948bc1c7ef56584e9c2372
Kyrylo <fresh.js.ftd.@gmail.com>
2023-08-08 18:42:05 +0300

    Add .env with examples

--- server/constants.js
+++ server/constants.js
@@ -1,15 +1,12 @@
-
-const USER_LOGIN = process.env.USER_LOGIN || 'admin';
-const USER_PASSWORD = process.env.USER_PASSWORD || 'admin';
-const DB_HOST = process.env.DB_HOST || 'mongotest.yuttpkc.mongodb.net';
+require('dotenv').config();
 
 const CONSTANTS = {
   PORT: process.env.PORT || 5000,
   DB_URL: process.env.DB_URL ||
-  `mongodb+srv://${USER_LOGIN}:${USER_PASSWORD}@${DB_HOST}`,
-  DB_NAME : process.env.DB_NAME || 'chatDB',
-  ACCESS_TOKEN_SECRET: 'ddfdsfgfgfdsftgre',
-  ACCESS_TOKEN_EXPIRES_IN: 600,
+    `mongodb://localhost:27017`,
+  DB_NAME: process.env.DB_NAME || 'chatDB',
+  ACCESS_TOKEN_SECRET: process.env.ACCESS_TOKEN_SECRET,
+  ACCESS_TOKEN_EXPIRES_IN: process.env.ACCESS_TOKEN_EXPIRES_IN,
 }
 
 module.exports = CONSTANTS;
