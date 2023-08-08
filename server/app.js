b88d1c854988a63dc473372c07753788aa59e185
Kyrylo <fresh.js.ftd.@gmail.com>
2023-08-08 19:29:10 +0300

    Add cors

--- server/app.js
+++ server/app.js
@@ -1,10 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const router = require('./routers');
 const { basicErrorHandler } = require('./middlewares/errors');
 const { tokenErrorHandler } = require('./middlewares/errors/tokens');
 
 const app = express();
-
+app.use(cors());
 app.use(express.json());
 app.use(router);
 
