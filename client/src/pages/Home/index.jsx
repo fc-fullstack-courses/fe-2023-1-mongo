90a74bc1a34028393f803834fe26e06902e25e24
Kyrylo <fresh.js.ftd.@gmail.com>
2023-08-08 19:55:51 +0300

    Add basic user display

--- client/src/pages/Home/index.jsx
+++ client/src/pages/Home/index.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import Header from '../../components/Header';
+import { useSelector } from 'react-redux';
 
 const HomePage = () => {
+  const { user } = useSelector((state) => state.auth);
   return (
     <div>
       <Header />
       <h1>Home Page</h1>
+      <p>
+        <pre>{JSON.stringify(user, null, 4)}</pre>
+      </p>
     </div>
   );
 };
