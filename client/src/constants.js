58bfdb0dc9f0482a3008b90099601f9fe9a82a1b
Kyrylo <fresh.js.ftd.@gmail.com>
2023-08-11 20:56:11 +0300

    Add docker compose example

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,9 +1,9 @@
 // ngrok-skip-browser-warning
-const SERVER_URL = 'd0db-5-180-128-9.ngrok-free.app';
+const SERVER_URL = 'localhost:9999';
 
 const CONSTANTS = {
-  HTTP_BASE_URL: `https://${SERVER_URL}`,
-  WS_SERVER_URL: `wss://${SERVER_URL}`,
+  HTTP_BASE_URL: `http://${SERVER_URL}`,
+  WS_SERVER_URL: `ws://${SERVER_URL}`,
   TOKEN_STRING: 'token',
   SOCKET_EVENTS: {
     NEW_MESSAGE: 'newMessage',
